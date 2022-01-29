d6309c115b6e404faba7e047f82e6788f5b6a6f3
Shoaib Shahriar <shoaibshahriar29@hotmail.com>
2022-01-30 01:58:52 +0600

    Code First approach for creating the database with data formation.

---
+++ CRUD.Database/Migrations/20220129194711_initialmigration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD.Database.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DeptId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeptName = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DeptId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(maxLength: 50, nullable: false),
+                    EmpAge = table.Column<int>(nullable: false),
+                    EmpGender = table.Column<string>(nullable: true),
+                    DeptId = table.Column<int>(nullable: false),
+                    EmpEmail = table.Column<string>(maxLength: 50, nullable: true),
+                    EmpPhoto = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmpId);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department",
+                        column: x => x.DeptId,
+                        principalTable: "Department",
+                        principalColumn: "DeptId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DeptId",
+                table: "Employee",
+                column: "DeptId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
---
+++ CRUD.Database/ReadMe.txt
@@ -0,0 +1,9 @@
+﻿Code First Approach
+-------------------
+As this project was made with the DBFirst approach, I would needed to create a new DB and Tables to work this project perfectly. 
+Then I thought CodeFirst Approach will come handy this time. The commands are written below. 
+> To create a new migration, which will create a migration folder with migration name. 
+add-migration initialmigration 
+> If connection string OK then
+update-database
+
